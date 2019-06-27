11a5526dac864f78936755128861a70dd16e138f
espoelstra <7798975+espoelstra@users.noreply.github.com>
2019-06-26 19:28:28 -0500

    Fixed python and java

--- java/src/main/java/com/example/Main.java
+++ java/src/main/java/com/example/Main.java
@@ -7,5 +7,9 @@ public class Main {
     public static void main(String[] args) {
         Joiner joiner = Joiner.on(" ");
         System.out.println(joiner.join("Which process", "can I pipe some coffee from?"));
+      System.out.print("Java Specification Version: ");
+      System.out.println(System.getProperty("java.specification.version"));
+      System.out.print("java Runtime Environment (JRE) version: ");
+      System.out.println(System.getProperty("java.version"));
     }
 }
--- python/tests/my_test.py
+++ python/tests/my_test.py
@@ -2,4 +2,4 @@ from mypackage.run import is_this_just_fantasy
 
 
 def test_is_this_the_real_life():
-    assert not is_this_just_fantasy()
+    assert is_this_just_fantasy()
